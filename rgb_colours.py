af16aeeefd83bc880b61c5160af9760ecaeb6751
Providence-o <97491889+Providence-o@users.noreply.github.com>
2025-05-29 13:17:42 +0100

    write main program

--- rgb_colours.py
+++ rgb_colours.py
@@ -1,18 +1,4 @@
-# This program knows about the RGB code corresponding to common colours.
-#
-# Usage:
-#
-# $ python rgb_colours.py [colour]
-#
-# For instance:
-#
-# $ python rgb_colours.py red
-# The RGB code for red is F00
-#
-# or:
-#
-# $ python rgb_colours.py "burnt sienna"
-# I don't know the RGB code for burnt sienna
+import argparse
 
 colours = [
     ['red', 'F00'],
@@ -23,7 +9,31 @@ colours = [
     ['magenta', 'F0F'],
 ]
 
+description = 'This program knows about the RGB code corresponding to common colours.'
+
+def main(colour_args):
+    print(get_rgb_code(colour_args))
+
+def get_rgb_code(colour_args):
+    result = [rgb_code for colour, rgb_code in colours_dict().items() if colour == colour_args]
+
+    if result:
+        return f"The RGB code for {colour_args} is {result[0]}"       
+    else:
+        return f"I don't know the RGB code for {colour_args}"
+
 def colours_dict():
     return dict(colours)
 
-print(colours_dict())
+def run():
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("colour_name", type=str, help="Gets RGB code for a given colour")
+    args = parser.parse_args()
+    colour_args = args.colour_name
+   
+    main(colour_args)
+
+
+if __name__ == "__main__":
+    run()
+ 
