0edcc47ad5698dfce846ad7f08f2cbe81bba5794
Providence-o <97491889+Providence-o@users.noreply.github.com>
2025-05-29 13:27:19 +0100

    extra: enable either the colour or rgb code to be used as a program argument

--- rgb_colours.py
+++ rgb_colours.py
@@ -12,20 +12,22 @@ colours = [
 description = 'This program knows about the RGB code corresponding to common colours.'
 
 def main(colour_args):
-    print(get_rgb_code(colour_args))
+    print(get_colour_match_result(colour_args))
 
-def get_rgb_code(colour_args):
-    matched_colour = matching_colour(colour_args)
+def get_colour_match_result(colour_args):
+    matched_value = matching_value(colour_args)
     
-    if not matched_colour:
+    if not matched_value:
         return f"I don't know the RGB code for {colour_args}"
-    return f"The RGB code for {colour_args} is {matched_colour}"
+    return matched_value
     
-def matching_colour(colour_args):
-    result = [rgb_code for colour, rgb_code in colours_dict().items() if colour.lower() == colour_args.lower()]
-    
-    if result:
-        return result[0]
+def matching_value(colour_args):
+    for colour, rgb_code in colours_dict().items():
+        if colour.lower() == colour_args.lower():
+            return f"The RGB code for {colour_args} is #{rgb_code}"
+        elif rgb_code.upper() == colour_args.upper():
+            return f"The colour name for #{colour_args} is {colour}"
+
 
 def colours_dict():
     return dict(colours)
