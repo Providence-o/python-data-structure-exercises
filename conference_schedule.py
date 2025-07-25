9b9b0d9e8f4ad23617aef08cf7147e782784f184
Providence-o <97491889+Providence-o@users.noreply.github.com>
2025-07-25 14:38:42 +0100

    get code to run

--- conference_schedule.py
+++ conference_schedule.py
@@ -11,8 +11,9 @@
 # $ python conference_schedule.py "Main Hall" 13:30
 # 
 
+import sys
 
-schedule = {
+SCHEDULE = {
     'Main Hall': {
         '10:00': 'Django REST framework',
         '11:00': 'Lessons learned from PHP',
@@ -39,11 +40,68 @@ schedule = {
     },
 }
 
-print('There are talks scheduled in {} rooms'.format(len(schedule)))
-
+# print('There are talks scheduled in {} rooms'.format(len(schedule)))
 
 # TODO:
 # * Implement the program as described in the comments at the top of the file.
+CONFERENCE_START = '10:00'
+CONFERENCE_END = '17:00'
+
+def main():
+    print(get_conference_information())
+
+def get_conference_information():
+    parsed = parser()
+    if len(parsed) == 2:
+        room, time = parsed
+        return get_session(room, time)
+    elif len(parsed) == 1:
+        talk = parsed[0]
+        return get_room_and_time(talk)
+    
+    return parsed
+
+def round_down_time(time):
+    hour, _ = time.split(":")
+    return f"{hour}:00"
+
+def parser():
+    if len(sys.argv) == 2:
+        session_title = sys.argv[1]
+        return (session_title,)
+    elif len(sys.argv) == 3:
+        room = sys.argv[1]
+        time = sys.argv[2]
+        return room, time
+    else:
+        return "Usage: \n\npython conference_schedule.py '<room>' '<time>' \nor \npython conference_schedule.py '<title of session>'"
+    
+
+def get_session(room_arg, time_arg):
+    DEFAULT_SESSION = ""
+
+    if not CONFERENCE_START <= time_arg < CONFERENCE_END:
+        return "Please enter a valid time. Conference runs from 10:00 - 16:00"
+    time_per_hour = round_down_time(time_arg)
+    current_session = SCHEDULE.get(room_arg, {}).get(time_per_hour, DEFAULT_SESSION)
+    if current_session == DEFAULT_SESSION:
+        return "Enter a valid room name"
+    else:
+        return f"The current session is '{current_session}'"
+
+def get_room_and_time(talk_arg):
+    for room, schedule in SCHEDULE.items():
+        for time, talk in schedule.items():
+            if talk_arg in talk:
+                return f"'{talk}' is happening at {time} in {room}"
+    return f"'{talk_arg}' not found"
+    
+
+if __name__ == "__main__":
+    main()
+    
+
+
 
 # TODO (extra):
 # * Change the program so that that it can tell you what session is running in
