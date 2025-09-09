4c947068235c1ab998541e102019b88186ee8428
Providence-o <97491889+Providence-o@users.noreply.github.com>
2025-09-09 12:46:05 +0100

    create class instance to store data

--- browser_stats.py
+++ browser_stats.py
@@ -22,33 +22,29 @@ from browser_stats_data import browser_stats_by_year_and_month
 #   * Which month saw the biggest percentage point 
 
 def main():
-    # this function runs the function that will print the report e.g print(some_function)
-    
     print(report_content())
 
 
 def report_content():
-    # this function contains the strings when variables to create the data are passed/called
-    report = CreateReport().analysis() # an instance of the CreateReport class
+    report = CreateReport(browser_stats_by_year_and_month).analysis() 
 
-    report_body = f"""
+    return f"""
         The report covers {report["period_start"]} to {report["period_end"]}
         In the period covered, '{report["browsers_over_50"]}' has had over 50% of market share
         In {report["firefox_early_popularity"]} firefox first became the most popular browser
         Chrome first overtook IE in popularity in {report["chrome_overtake_ie"]}
         Firefox's popularity was highest in {report["firefox_highest_popularity"]}
         In which month was the combined popularity of Safari and Opera highest? {report["safari_opera_highest"]}
-        In {report["chrome_percentage_rise"]}, Chrome had the biggest percentage point rise in poprularity"""
+        In {report["chrome_percentage_rise"]}, Chrome had the biggest percentage point rise in popularity"""
     
-    return report_body
-
-
 
 class CreateReport:
+    def __init__(self, browser_stats):
+        self.browser_stats = browser_stats
 
     def data(self):
         flat_structure = []
-        for year, value in browser_stats_by_year_and_month.items():
+        for year, value in self.browser_stats.items():
             for month, browser in value.items():
                 flat_structure.append((year, month, browser["Chrome"], browser["IE"], browser["Firefox"], browser["Safari"], browser["Opera"]))
         
@@ -63,51 +59,52 @@ class CreateReport:
 
     
     def analysis(self):
-        
+        data = self.data()
+
+        id = data["id"]
+        safari = data["browsers"]["safari"]
+        opera = data["browsers"]["opera"]
+        chrome = data["browsers"]["chrome"]
+        ie = data["browsers"]["ie"]
+        firefox = data["browsers"]["firefox"]
+        browser_data = data["browsers"]
+        month = data["month"]
+        year = data["year"]
+
         #   * In the period covered, which browsers have had over 50% of market share?
-        browser_data = self.data()["browsers"]
         browsers_over_50 = [browser_name for browser_name, browser_stat in browser_data.items() if max(browser_stat) > 50]
 
         #   * In which month was Firefox's popularity highest?
-        firefox = self.data()["browsers"]["firefox"]
-        f_index = firefox.index(max(firefox))
-        firefox_highest_popularity = self.data()["month"][f_index]
+        firefox_max = firefox.index(max(firefox))
+        firefox_highest_popularity = month[firefox_max]
 
-        # * In which month was the combined popularity of Safari and Opera highest?
-        id = self.data()["id"]
-        safari = self.data()["browsers"]["safari"]
-        opera = self.data()["browsers"]["opera"]
-        chrome = self.data()["browsers"]["chrome"]
-        ie = self.data()["browsers"]["ie"]
-
-        combined_dict = {id[i]: safari[i] + opera[i] for i in range(len(id))}
-        high_id = max(combined_dict, key=combined_dict.get)
-        safari_opera_highest = self.data()["month"][high_id]
+        #   * In which month was the combined popularity of Safari and Opera highest?
+        safari_opera_sum = {id[i]: safari[i] + opera[i] for i in range(len(id))}
+        safari_opera_max = max(safari_opera_sum, key=safari_opera_sum.get)
+        safari_opera_highest = month[safari_opera_max]
         
 
         #   * In which month did Chrome first overtake IE in popularity? 
-        chrome_ie_dict = [id[i] for i in range(len(id)) if chrome[i] > ie[i]]
-        highest_chrome_ie = max(chrome_ie_dict)
-        chrome_overtake_ie = self.data()["month"][highest_chrome_ie]
+        chrome_ie_popularity = [id[i] for i in range(len(id)) if chrome[i] > ie[i]]
+        chrome_ie_max = max(chrome_ie_popularity)
+        chrome_overtake_ie = month[chrome_ie_max]
         
         #   * In which month did Firefox first become the most popular browser?
-        fire_dict = [id[i] for i in range(len(id)) if firefox[i] > ie[i] and safari[i] and opera[i] and chrome[i]]
-        highest_fire = max(fire_dict)
-        firefox_early_popularity = self.data()["month"][highest_fire]
+        firefox_popularity = [id[i] for i in range(len(id)) if firefox[i] > ie[i] and safari[i] and opera[i] and chrome[i]]
+        firefox_popularity_max = max(firefox_popularity)
+        firefox_early_popularity = month[firefox_popularity_max]
     
         #   * Which month saw the biggest percentage point rise in Chrome's popularity?
-        chrome_percent = {id[i]: chrome[i] - chrome[i + 1] for i in range(len(id) - 1)}
-        perc_id = max(chrome_percent, key=chrome_percent.get)
-        chrome_percentage_rise = self.data()["month"][perc_id]
+        chrome_percent_increase = {id[i]: chrome[i] - chrome[i + 1] for i in range(len(id) - 1)}
+        max_of_increase = max(chrome_percent_increase, key=chrome_percent_increase.get)
+        chrome_percentage_rise = month[max_of_increase]
 
         #   * What period does the report cover?
-        month = self.data()["month"]
-        year = self.data()["year"]
-        max_id = max(id)
-        min_id = min(id)
+        maximum_id = max(id)
+        minimum_id = min(id)
         
-        period_start = f"{month[max_id]} {year[max_id]}"
-        period_end = f"{month[min_id]} {year[min_id]}"
+        period_start = f"{month[maximum_id]} {year[maximum_id]}"
+        period_end = f"{month[minimum_id]} {year[minimum_id]}"
            
         return {
             "browsers_over_50": ', '.join(browsers_over_50),
@@ -119,8 +116,6 @@ class CreateReport:
             "period_start": period_start,
             "period_end": period_end
         }
-
-
 
 if __name__ == "__main__":
     main()
